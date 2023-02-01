4d589fd1b198a6b91b5506dc85330814f23852de
odelolajosh <oladapoodelola@gmail.com>
2023-02-01 21:33:06 +0100

    feat: regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str):
+    """ Returns the log message obfuscated. """
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
