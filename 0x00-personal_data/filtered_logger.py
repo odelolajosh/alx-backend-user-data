c67c17802b902949c5ce5821217bc9399e988eeb
odelolajosh <oladapoodelola@gmail.com>
2023-02-01 21:34:55 +0100

    chore: annotate function properly

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """ Returns the log message obfuscated. """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
