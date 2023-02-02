601f6aa5434cba424a3eec44891765a3c703ac6d
odelolajosh <oladapoodelola@gmail.com>
2023-02-03 00:21:17 +0100

    chore: annotate function properly

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,11 @@
 from typing import List
 import re
 import logging
+import os
+import mysql.connector
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -12,6 +17,48 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a logging.Logger object. """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to the database. """
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    return mysql.connector.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=db_name
+    )
+
+
+def main():
+    """ Obtain a database connection using get_db and retrieve all rows in the
+        users table and display each row under a filtered format. """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        message = ""
+        for field in row:
+            message += f"{field};"
+        logger.info(message)
+    cursor.close()
+    db.close()
+
 
 
 class RedactingFormatter(logging.Formatter):
@@ -29,3 +76,7 @@ class RedactingFormatter(logging.Formatter):
         """ Formats a log record and obfuscates fields. """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
