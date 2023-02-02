aac57013a3746c40d156cde03bfb18d89a852766
odelolajosh <oladapoodelola@gmail.com>
2023-02-03 00:27:11 +0100

    feat: RedactingFormatter

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Encrypting passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string. """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Returns True if the password is valid for the given hashed password.
+        Otherwise, it returns False. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
