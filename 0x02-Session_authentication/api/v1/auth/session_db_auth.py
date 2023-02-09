5b2419639c9f8cc4944d1a3ce9a9a936e9fac027
odelolajosh <oladapoodelola@gmail.com>
2023-02-09 22:24:05 +0100

    feat: logout

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+This module contains the class SessionDBAuth
+"""
+from .session_exp_auth import SessionExpAuth
+from os import getenv
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ SessionDBAuth Class. """
+
+    def create_session(self, user_id: str = None):
+        """ Create a session. """
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        from models.user_session import UserSession
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None):
+        """ Returns a User ID based on a Session ID """
+        if not session_id:
+            return None
+        from models.user_session import UserSession
+        user_session = UserSession.search({'session_id': session_id})
+        if not user_session:
+            return None
+        if self.session_duration <= 0:
+            return user_session[0].user_id
+        if user_session[0].created_at + timedelta(seconds=self.session_duration) < datetime.now():
+            return None
+        return user_session[0].user_id
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout. """
+        if not request:
+            return False
+        session_cookie = self.session_cookie(request)
+        if not session_cookie:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if not user_id:
+            return False
+        from models.user_session import UserSession
+        user_session = UserSession.search({'session_id': session_cookie})
+        if not user_session:
+            return False
+        user_session[0].remove()
+        return True
---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+This module contains the class SessionAuth
+"""
+from .session_auth import SessionAuth
+from os import getenv
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """ SessionExpAuth Class. """
+
+    def __init__(self):
+        """ Loads important instance attributes. """
+        try:
+            self.session_duration = int(getenv('SESSION_DURATION'))
+        except Exception:
+            self.session_duration = 0
+
+    def create_session(self, user_id: str = None):
+        """ Create a session. """
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        user_id = self.user_id_by_session_id[session_id]
+        self.user_id_by_session_id[session_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None):
+        """ Returns a User ID based on a Session ID """
+        if not session_id:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        session_dict = self.user_id_by_session_id.get(session_id, None)
+        if not session_dict:
+            return None
+        if self.session_duration <= 0:
+            return session_dict['user_id']
+        if 'created_at' not in session_dict:
+            return None
+        if session_dict['created_at'] + timedelta(seconds=self.session_duration) < datetime.now():
+            return None
