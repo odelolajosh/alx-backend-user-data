f9a15a85a6ea7d454f68466f7a9035195e8b5d0a
odelolajosh <oladapoodelola@gmail.com>
2023-02-09 20:29:03 +0100

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,7 +29,7 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         user_id = self.user_id_for_session_id(self.session_cookie(request))
-        return  User.get(user_id)
+        return User.get(user_id)
