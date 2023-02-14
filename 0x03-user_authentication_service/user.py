bac7adf9610c72be9c2da0204959d4e527547537
odelolajosh <oladapoodelola@gmail.com>
2023-02-14 13:55:52 +0100

    fix: lint

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class User(Base):
     """ User model. """
     __tablename__ = "users"
