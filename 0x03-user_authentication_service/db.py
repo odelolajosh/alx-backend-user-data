0ef1b24bd75b5d17fe5ff7beda4263fcf09725a2
odelolajosh <oladapoodelola@gmail.com>
2023-02-16 14:16:21 +0100

    fix: lint

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
