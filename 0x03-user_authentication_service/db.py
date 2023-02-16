c145374add3e6b760726b1c890d0e7ce274fce88
odelolajosh <oladapoodelola@gmail.com>
2023-02-16 14:19:14 +0100

    fix: handle exception

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+        except Exception:
+            self._session.rollback()
+            return None
