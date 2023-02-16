b145a3949f533e1f284f202966c30b3edd82db3d
odelolajosh <oladapoodelola@gmail.com>
2023-02-16 14:42:45 +0100

    feat: find iser by filter

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 from user import Base, User
 
@@ -41,3 +44,16 @@ class DB:
         except Exception:
             self._session.rollback()
             return None
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given keyword argument
+        """
+        for key in kwargs:
+            if not hasattr(User, key):
+                raise InvalidRequestError()
+
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound()
+
+        return result
