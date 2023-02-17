fae46e43ae2e9589156516a5a2ea4d7320ef512b
odelolajosh <oladapoodelola@gmail.com>
2023-02-17 13:27:02 +0100

    feat: destorys session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,8 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int):
+        """ Destroys a session. """
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
