78d06e6e766eef604e55bded6cd477ea06ebfc53
odelolajosh <oladapoodelola@gmail.com>
2023-02-17 12:34:15 +0100

    feat: register user

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Auth. """
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """ Hashes a password. """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        if not email or not password:
+            return None
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            pass
+        except InvalidRequestError:
+            return None
+
+        hashed_pwd = _hash_password(password).decode("utf-8")
+        user = self._db.add_user(email, hashed_pwd)
+        return user
