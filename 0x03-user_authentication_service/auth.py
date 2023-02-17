73e7d4f7a7867fe7dadb7fbfc6540b75d650b937
odelolajosh <oladapoodelola@gmail.com>
2023-02-17 13:23:53 +0100

    feat: find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 import uuid
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Optional
 from db import DB
 from user import User
 
@@ -60,3 +61,10 @@ class Auth:
             return session_id
         except NoResultFound:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """ Return the User corresponding to a session id. """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
