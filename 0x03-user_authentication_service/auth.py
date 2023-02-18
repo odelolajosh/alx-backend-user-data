487bcf4e68437c4db227087ec4a1244b36e34eb7
odelolajosh <oladapoodelola@gmail.com>
2023-02-18 07:20:10 +0100

    chore: add docstring to method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,6 +27,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """ Registers a new user. """
         if not email or not password:
             return None
         try:
