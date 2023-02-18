d1aa9991de05a60d944208a6621c65ec3fbc3d86
odelolajosh <oladapoodelola@gmail.com>
2023-02-18 07:11:18 +0100

    refactor: rename auth instance

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,7 +60,7 @@ def logout():
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
